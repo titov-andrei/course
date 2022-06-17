88029cdd93e994a7595c75b10598e079c4573936
titov-andrei <titov_andrei@mail.ru>
2022-06-17 15:28:54 +0300

    Update Posts.php

--- app/Http/Controllers/Posts.php
+++ app/Http/Controllers/Posts.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class Posts extends Controller
 {
     public function index() {
+
+        (new Post())->save();
+
         return view('posts.index', [
             'rand' => mt_rand(0, 10),
             'time' => time(),
