b2041bbbff6acf66d93ce518ec1fe96a0d2ce741
titov-andrei <titov_andrei@mail.ru>
2022-06-18 17:40:38 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,5 @@ use App\Http\Controllers\Posts;
 // });
 
 Route::get('/posts', [ Posts::class, 'index' ]);
-Route::post('/posts/create', [ Posts::class, 'create' ]);
+Route::get('/posts/create', [ Posts::class, 'create' ]);
 Route::post('/posts', [ Posts::class, 'store' ]);
