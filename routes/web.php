9720af954bc7e4171aa2911aeb443c5c355d4f77
titov-andrei <titov_andrei@mail.ru>
2022-06-18 13:19:26 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Posts;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/hi', [ Posts::class, 'index' ]);
+Route::get('/', [ Posts::class, 'index' ]);
