8eb6ef53520a414909bc52efd493a9df3899ca7f
titov-andrei <titov_andrei@mail.ru>
2022-06-18 21:10:43 +0300

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,7 @@
 <hr>
 @foreach ($posts as $post)
     <h2>{{ $post->title }}</h2>
-    <em>{{ $post->created_at }}</em>
+    <em>{{ $post->created_at }}</em><br>
+    <a href="/posts/{{ $post->id }}">more...</a>
     <hr>
 @endforeach
