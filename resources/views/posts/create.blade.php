3ce8e4f786983be59c3c7f55709561dc27c36288
titov-andrei <titov_andrei@mail.ru>
2022-06-18 18:23:47 +0300

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,4 +1,5 @@
 <form action="/posts" method="post">
+    @csrf
     Title--: <input type="text" name="title" /><br>
     Content: <textarea name="content"></textarea><br>
     <button>Send</button>
