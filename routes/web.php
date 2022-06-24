6e89a54e03baaaefcd7ca4aaad83fe81353b6300
titov-andrei <titov_andrei@mail.ru>
2022-06-24 20:54:03 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,8 @@ Route::get('/posts', [ PostsController::class, 'index' ]);
 Route::get('/posts/create', [ PostsController::class, 'create' ]);
 Route::get('/posts/{id}', [ PostsController::class, 'show' ]);
 Route::post('/posts', [ PostsController::class, 'store' ]);
+
+Route::get('/categories', [ CategoriesController::class, 'index' ]);
+Route::get('/posts/create', [ CategoriesController::class, 'create' ]);
+Route::get('/posts/{id}', [ CategoriesController::class, 'show' ]);
+Route::post('/posts', [ CategoriesController::class, 'store' ]);
