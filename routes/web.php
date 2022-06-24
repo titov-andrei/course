f61d6c5f7b9910c1865722b3c1dae0b1e658992a
titov-andrei <titov_andrei@mail.ru>
2022-06-24 21:45:26 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,20 @@ Route::get('/posts/create', [ PostsController::class, 'create' ]);
 Route::get('/posts/{id}', [ PostsController::class, 'show' ]);
 Route::post('/posts', [ PostsController::class, 'store' ]);
 
-Route::get('/categories', [ CategoriesController::class, 'index' ]);
-Route::get('/categories/create', [ CategoriesController::class, 'create' ]);
-Route::get('/categories/{id}', [ CategoriesController::class, 'show' ]);
-Route::post('/categories', [ CategoriesController::class, 'store' ]);
-Route::get('/categories/{id}/edit', [ CategoriesController::class, 'edit' ]);
-Route::put('/categories/{id}', [ CategoriesController::class, 'update' ]);
-Route::delete('/categories/{id}', [ CategoriesController::class, 'destroy' ]);
+// Route::get('/categories', [ CategoriesController::class, 'index' ]);
+// Route::get('/categories/create', [ CategoriesController::class, 'create' ]);
+// Route::get('/categories/{id}', [ CategoriesController::class, 'show' ]);
+// Route::post('/categories', [ CategoriesController::class, 'store' ]);
+// Route::get('/categories/{id}/edit', [ CategoriesController::class, 'edit' ]);
+// Route::put('/categories/{id}', [ CategoriesController::class, 'update' ]);
+// Route::delete('/categories/{id}', [ CategoriesController::class, 'destroy' ]);
+
+Route::prefix('/categories')->controller(CategoriesController::class)->group(function(){
+    Route::get('/', 'index');
+    Route::get('/create', 'create');
+    Route::get('/{id}', 'show');
+    Route::post('/', 'store');
+    Route::get('/{id}/edit', 'edit');
+    Route::put('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
+});
